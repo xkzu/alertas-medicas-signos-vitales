923ba907e9690c317c98a5c90a677bd7106e4f6a
Kzu <alvaro.vega.pavez@gmail.com>
2025-01-10 05:49:48 -0300

    add: main class

---
+++ src/main/java/com/alertasmedicas/app/signo/SignoApplication.java
@@ -0,0 +1,13 @@
+package com.alertasmedicas.app.signo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignoApplication.class, args);
+	}
+
+}
