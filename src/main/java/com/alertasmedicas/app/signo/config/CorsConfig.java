1307c094c9032e0f96fdae3330250e98acad7aba
Kzu <alvaro.vega.pavez@gmail.com>
2025-01-13 03:33:56 -0300

    update: se indica que es una clase de config

--- src/main/java/com/alertasmedicas/app/signo/config/CorsConfig.java
+++ src/main/java/com/alertasmedicas/app/signo/config/CorsConfig.java
@@ -2,10 +2,12 @@ package com.alertasmedicas.app.signo.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig {
 
     @Value("${cors.allowed-origins:}")
